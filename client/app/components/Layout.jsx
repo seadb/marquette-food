38aef3c0b3fd329e357c055198c6bc5c02bca691
Chelsea Bridson <bridsonc@msu.edu>
2016-11-11 12:23:30 -0500

    Convert components to functions rather than classes this simplifies the code. can always add classes back in if we need them later - client/app/components/Layout.jsx   convert to function - client/app/components/Logo.jsx   convert to function - client/app/components/Nav.jsx   convert to function - client/app/components/Routes.jsx   convert to function

--- client/app/components/Layout.jsx
+++ client/app/components/Layout.jsx
@@ -1,22 +1,17 @@
-import React, { Component, PropTypes } from 'react'
-import { connect } from 'react-redux'
+import React from 'react'
 import Routes from '../components/Routes';
 import Logo from '../components/Logo';
 import Nav from '../components/Nav';
 import styles from '../styles.css';
- 
-class Layout extends Component {
-  render() {
-    return (
-      <div>
-        <div className={styles.header} >
-          <Logo/>
-          <Nav/>
-        </div>
-        <Routes />
-      </div>
-    )
-  }
-}
+
+const Layout = () => (
+  <div>
+    <div className={styles.header} >
+      <Logo/>
+      <Nav/>
+    </div>
+    <Routes />
+  </div>
+)
 
 export default Layout
--- client/app/components/Routes.jsx
+++ client/app/components/Routes.jsx
@@ -1,17 +1,13 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Router, Route, IndexRoute, hashHistory, IndexLink, Link } from 'react-router';
 import Farms from '../../farm';
 import Index from '../../Index';
 
-class Routes extends Component {
-  render() {
-    return (
-      <Router history={hashHistory}>
-        <Route path='/' component={Index} />
-        <Route path='/farms' component={Farms} />
-      </Router>
-    );
-  }
-}
+const Routes = () => (
+  <Router history={hashHistory}>
+    <Route path='/' component={Index} />
+    <Route path='/farms' component={Farms} />
+  </Router>
+)
 
 module.exports = Routes
